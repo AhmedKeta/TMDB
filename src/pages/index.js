12311a7f0aee48e1f4f2aeb96f88130737173271
keta <ahmedketa12@gmail.com>
2023-06-12 21:54:32 +0300

    Language

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,12 @@
-// import Favourites from "./Favourites/Favourites";
-// import Home from "./Home/Home";
-// import Login from "./Login/Login";
-// import Movies from "./Movies/Movies";
-// import NotFound from "./NotFound/NotFound";
-// import Profile from "./Profile/Profile";
-// import Register from "./Register/Register";
-// import Search from "./Search/Search";
-// import Movie from "./Movie/Movie";
-// export {
-//   Favourites,
-//   Home,
-//   Login,
-//   Movies,
-//   NotFound,
-//   Profile,
-//   Register,
-//   Search,
-//   Movie,
-// };
-
-import { lazy } from "react";
-
-const Favourites = lazy(() => import("./Favourites/Favourites"));
-const Home = lazy(() => import("./Home/Home"));
-const Login = lazy(() => import("./Login/Login"));
-const Movies = lazy(() => import("./Movies/Movies"));
-const NotFound = lazy(() => import("./NotFound/NotFound"));
-const Profile = lazy(() => import("./Profile/Profile"));
-const Register = lazy(() => import("./Register/Register"));
-const Search = lazy(() => import("./Search/Search"));
-const Movie = lazy(() => import("./Movie/Movie"));
+import Favourites from "./Favourites/Favourites";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Movies from "./Movies/Movies";
+import NotFound from "./NotFound/NotFound";
+import Profile from "./Profile/Profile";
+import Register from "./Register/Register";
+import Search from "./Search/Search";
+import Movie from "./Movie/Movie";
 export {
   Favourites,
   Home,
@@ -41,3 +18,26 @@ export {
   Search,
   Movie,
 };
+
+// import { lazy } from "react";
+
+// const Favourites = lazy(() => import("./Favourites/Favourites"));
+// const Home = lazy(() => import("./Home/Home"));
+// const Login = lazy(() => import("./Login/Login"));
+// const Movies = lazy(() => import("./Movies/Movies"));
+// const NotFound = lazy(() => import("./NotFound/NotFound"));
+// const Profile = lazy(() => import("./Profile/Profile"));
+// const Register = lazy(() => import("./Register/Register"));
+// const Search = lazy(() => import("./Search/Search"));
+// const Movie = lazy(() => import("./Movie/Movie"));
+// export {
+//   Favourites,
+//   Home,
+//   Login,
+//   Movies,
+//   NotFound,
+//   Profile,
+//   Register,
+//   Search,
+//   Movie,
+// };
